80f6a325a1cd37b9d1970dc6439732407fb16bfa
olyao23 <101214018+olyao23@users.noreply.github.com>
2022-03-12 13:49:50 -0300

    Added files

---
+++ src/main/java/mk/ukim/finki/plannerwp/PlannerWpApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.plannerwp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerWpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerWpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/plannerwp/PlannerWpApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.plannerwp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerWpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
