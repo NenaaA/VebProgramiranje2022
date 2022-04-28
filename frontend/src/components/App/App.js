e2d864cb5b76191bcd573b09187cbd75b8ebdb6f
AnastasovaN <nena.anastasova@stage.alosys.it>
2022-04-28 14:01:58 +0200

    Add routes to home and daily task

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,13 +1,22 @@
 import HomeLayout from "../../ui/layout/home-layout/HomeLayout";
 import Task from "../task/task";
+import {Route, Router} from "react-router-dom";
+import {Home} from "@mui/icons-material";
 
 function App() {
     return (
-        <>
-            <HomeLayout>
-                <Task/>
-            </HomeLayout>
-        </>
+        <Router>
+            <Route path={"/home"}>
+                <HomeLayout>
+                    <Home/>
+                </HomeLayout>
+            </Route>
+            <Route path={"/daily-tasks"}>
+                <HomeLayout>
+                    <Task/>
+                </HomeLayout>
+            </Route>
+        </Router>
     );
 }
 
--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -5,7 +5,7 @@ import React from "react";
 const HomePage = () => {
     return (
         <>
-            <Typography>Fuck this</Typography>
+            <Typography>Have all your tasks in one place</Typography>
         </>
     );
 }
