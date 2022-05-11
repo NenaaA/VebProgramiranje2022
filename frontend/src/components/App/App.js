c58fb760b523ac4d6ab3dda8444042d9e6cea7ec
olyao23 <101214018+olyao23@users.noreply.github.com>
2022-05-11 16:57:24 -0200

    Added Miscellaneous, In progress and Finished files, added routes in App.js

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -3,10 +3,39 @@ import DailyTasks from "../dailyTasks/dailyTasks";
 import {Route, Routes} from "react-router-dom";
 import HomePage from "../home/home";
 import React from "react";
+import Login from "../auth/login/login";
+import Logout from "../auth/logout/logout";
+import Register from "../auth/register/register";
+import AuthLayout from "../../ui/layout/auth-layout/AuthLayout";
 
 function App() {
     return (
         <Routes>
+            <Route path="/login" element={<AuthLayout>
+                <Login/>
+            </AuthLayout>}/>
+
+            <Route path="/logout" element={<HomeLayout>
+                <Logout/>
+            </HomeLayout>}/>
+
+            <Route path="/register" element={<AuthLayout>
+                <Register/>
+            </AuthLayout>}/>
+
+            <Route path="/miscellaneous" element={<AuthLayout>
+                <Register/>
+            </AuthLayout>}/>
+
+            <Route path="/inprogress" element={<AuthLayout>
+                <Register/>
+            </AuthLayout>}/>
+
+            <Route path="/finished" element={<AuthLayout>
+                <Register/>
+            </AuthLayout>}/>
+
+
             <Route path="/home" element={<React.Fragment>
                 <HomeLayout>
                     <HomePage/>
